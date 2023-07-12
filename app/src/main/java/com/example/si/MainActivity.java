1bbe74280950ef88289cef003861b01b75230fae
Doney Sajan <doney.sajan13@gmail.com>
2023-07-12 10:10:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/si/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.si;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void simpleinterest(View view) {
+
+    }
+}
